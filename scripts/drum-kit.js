4fe776b8ef731bf9af518ea39beb1bbbfb6c811b
Sawyer Ruben <30737015+sawyerru@users.noreply.github.com>
2019-08-26 10:30:27 -0400

    updated file path

--- scripts/drum-kit.js
+++ scripts/drum-kit.js
@@ -2,19 +2,19 @@
 function playSound(key) {
     switch (key) {
         case ('w'):
-            var sound = new Audio("drum-kit-sounds/tom-1.mp3");
+            var sound = new Audio("../images/drum-kit-sounds/tom-1.mp3");
             sound.play();
             break;
         case ('a'):
-            var sound = new Audio("drum-kit-sounds/tom-2.mp3");
+            var sound = new Audio("../images/drum-kit-sounds/tom-2.mp3");
             sound.play();
             break;
         case ('s'):
-            var sound = new Audio("drum-kit-sounds/tom-3.mp3");
+            var sound = new Audio("../images/drum-kit-sounds/tom-3.mp3");
             sound.play();
             break;
         case ('d'):
-            var sound = new Audio("drum-kit-sounds/tom-4.mp3");
+            var sound = new Audio("../images/drum-kit-sounds/tom-4.mp3");
             sound.play();
             break;
         case ('j'):
@@ -22,11 +22,11 @@ function playSound(key) {
             sound.play();
             break;
         case ('k'):
-            var sound = new Audio("drum-kit-sounds/kick-bass.mp3");
+            var sound = new Audio("../images/drum-kit-sounds/kick-bass.mp3");
             sound.play();
             break;
         case ('l'):
-            var sound = new Audio("drum-kit-sounds/snare.mp3");
+            var sound = new Audio("../images/drum-kit-sounds/snare.mp3");
             sound.play();
             break;
     }
