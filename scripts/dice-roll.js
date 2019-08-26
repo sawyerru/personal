d87f82a095219d0b17bf124c494518cd00041982
Sawyer Ruben <30737015+sawyerru@users.noreply.github.com>
2019-08-26 09:59:39 -0400

    adding JS

---
+++ scripts/dice-roll.js
@@ -0,0 +1,38 @@
+function randomNum(){
+    var num = Math.floor(Math.random() * 6);
+    // console.log(num);
+    return num + 1;
+}
+  
+function rollDice(){
+    //Player 1 action
+    var roll1 = randomNum();
+    var diceImg1 = "dice" + roll1 + ".png";
+    var location1 = "images/dice-imgs/" + diceImg1;
+    document.querySelectorAll("img")[0].setAttribute("src", location1);
+
+    //Player 2 action
+    var roll2 = randomNum();
+    var diceImg2 = "dice" + roll2 + ".png";
+    var location2 = "images/dice-imgs/" + diceImg2;
+    document.querySelectorAll("img")[1].setAttribute("src", location2);
+
+    console.log("Roll1: " + roll1 + "       Roll2: " + roll2);
+    if (roll1 > roll2){
+        //player1 wins
+        document.querySelector("p.top").innerHTML = "👑"
+        document.querySelector("p.top").style.textAlign = "left";
+        document.querySelector("p.top").style.paddingLeft = "1rem";
+    } else if (roll1 < roll2){
+        //player2 wins
+        document.querySelector("p.top").innerHTML = "👑"
+        document.querySelector("p.top").style.textAlign = "right";
+        document.querySelector("p.top").style.paddingRight = "1rem";
+    } else{
+        //tie
+        document.querySelector("p.top").innerHTML = "Tie! Roll Again.";
+        document.querySelector("p.top").style.textAlign = "center";
+    }
+    return;
+}
+
---
+++ scripts/drum-kit.js
@@ -0,0 +1,55 @@
+
+function playSound(key) {
+    switch (key) {
+        case ('w'):
+            var sound = new Audio("drum-kit-sounds/tom-1.mp3");
+            sound.play();
+            break;
+        case ('a'):
+            var sound = new Audio("drum-kit-sounds/tom-2.mp3");
+            sound.play();
+            break;
+        case ('s'):
+            var sound = new Audio("drum-kit-sounds/tom-3.mp3");
+            sound.play();
+            break;
+        case ('d'):
+            var sound = new Audio("drum-kit-sounds/tom-4.mp3");
+            sound.play();
+            break;
+        case ('j'):
+            var sound = new Audio("drum-kit-sounds/crash.mp3");
+            sound.play();
+            break;
+        case ('k'):
+            var sound = new Audio("drum-kit-sounds/kick-bass.mp3");
+            sound.play();
+            break;
+        case ('l'):
+            var sound = new Audio("drum-kit-sounds/snare.mp3");
+            sound.play();
+            break;
+    }
+}
+
+function buttonAnimation(key) {
+    document.querySelector("." + key).classList.add("pressed");
+    setTimeout(function () {
+        document.querySelector("." + key).classList.remove("pressed");
+    }, 200);
+}
+
+for (var i = 0; i < document.querySelectorAll("button.drum").length; i++) {
+
+    document.querySelectorAll("button.drum")[i].addEventListener("click", function () {
+        playSound(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+    });
+        
+
+    document.querySelectorAll("button.drum")[i].addEventListener("keydown", function (event) {
+        playSound(event.key);
+        buttonAnimation(event.key);
+    });
+}
+
